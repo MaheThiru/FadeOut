295f086f5cc0eac2b794d14e3e0d6f94b3a8ac5a
THIRUVADISAMY_P <ptsamy92@gmail.com>
2016-01-27 20:44:13 +0530

    sfds sf s fs

---
+++ FadeoutLib/PodViewController.h
@@ -0,0 +1,19 @@
+//
+//  PodViewController.h
+//  Pods
+//
+//  Created by THIRUVADISAMY_P on 25/01/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PodViewController : UIViewController
+
+// Method to Update the FadeOut View
+- (void) fadeOut:(UIView *)animationView timeInterval:(NSTimeInterval)timeDuration timeDelay:(NSTimeInterval)timeDelay fadeOutValue:(CGFloat) fadeOutOpacity;
+
+// Method to Update the FadeIn View
+- (void) fadeIn:(UIView *)animationView timeInterval:(NSTimeInterval)timeDuration timeDelay:(NSTimeInterval)timeDelay fadeOutValue:(CGFloat) fadeOutOpacity;
+
+@end
